e3e827cdfdd5dc91f1ba5d56e48398e86e5aa065
PabloRuizS2 <pabloruizs2002@gmail.com>
2022-05-31 18:06:26 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let numDeTabla= Number(prompt("ingresa el num de la tabla de multiplicar"))
+for(i =1; i <= 10; i++){
+    let res = numDeTabla * i;
+    alert("resultado" + numDeTabla + "*" + i + "=" + res)
+}
